ac18f860cd2c8553be4d65448f9263b44301967a
lonerpatti <53662439+lonerpatti@users.noreply.github.com>
2020-06-20 16:08:59 -0300

    Add Cache

--- src/mobx/index.tsx
+++ src/mobx/index.tsx
@@ -3,11 +3,13 @@ import {combustivel} from '../containers/combustivel/store';
 import {router} from './routes.store';
 import {starWars} from '../containers/star-wars/store';
 import {starWarsDetails} from '../containers/star-wars-details/store';
+import {cache} from '../containers/cache/store';
 
 export {
     router,
     combustivel,
     home,
     starWars,
-    starWarsDetails
+    starWarsDetails,
+    cache
   }
