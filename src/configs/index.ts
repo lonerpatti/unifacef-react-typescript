fa8affc3a9de15cdbbc42e07c802909a5520b357
lonerpatti <53662439+lonerpatti@users.noreply.github.com>
2020-07-04 17:22:34 -0300

    Ajustes diversos. Incluído uso da API Corona e Autenticação.

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -4,6 +4,7 @@ export const configs = {
       starWars: process.env.REACT_APP_STAR_WARS_URL,
       correios: process.env.REACT_APP_CEP_URL,
       github: process.env.REACT_APP_GITHUB_URL,
+      corona: process.env.REACT_APP_CORONA_URL,
     },
     sentry: process.env.REACT_APP_SENTRY_DSN,
     onesignal: process.env.REACT_APP_ONE_SIGNAL!
